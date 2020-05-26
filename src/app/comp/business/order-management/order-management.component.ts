6d4dd268adf930d0a4832e6e20fd25e403beeacc
phuongchinh <chinhbp010297@gmail.com>
2020-05-26 14:34:50 +0700

    update

--- src/app/comp/business/order-management/order-management.component.ts
+++ src/app/comp/business/order-management/order-management.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser'
-import {Router, ActivatedRoute} from '@angular/router'
+import { Router, ActivatedRoute } from '@angular/router'
 import { HttpConnectorService } from '../../../service/http-connector.service/http-connector.service.component'
 import { CONSUME_API } from '../../../service/services/consume-apis';
 import { FormGroup, Validators, FormControl } from "@angular/forms";
@@ -14,13 +14,62 @@ export class OrderManagementComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
+    private xhr: HttpConnectorService
   ) { }
 
   customerName: string;
   customerId: string;
   ngOnInit(): void {
     this.customerName = sessionStorage.getItem("customerName");
-    this.customerId = this.route.snapshot.paramMap.get("id");
+    this.customerId = this.route.snapshot.paramMap.get("customerId");
+    this.setupForm();
+    this.getOrderByCustomerId();
+  }
+
+  lstOrder: any = [];
+  formCreateOrder : FormGroup;
+  setupForm() {
+    this.formCreateOrder = new FormGroup({
+      name: new FormControl('', []),
+      desc: new FormControl('', []),
+      note: new FormControl('', []),
+      quantity: new FormControl('', []),
+      price: new FormControl('', [])
+    })
+  }
+
+  getOrderByCustomerId(){
+    let url = CONSUME_API.ORDER.GET_ORDER_BY_CUSTOMER_ID;
+    let param = {
+      'customerId': this.customerId
+    }
+    url += "?" + this.xhr.buildBodyParam(param);
+    this.xhr.get(url).subscribe((res: any) => {
+      if (res) {
+        this.lstOrder = res.result;
+      }
+    }, (err) => {
+
+    });
+  }
+  createOrder() {
+    let url = CONSUME_API.ORDER.CREATE_ORDER;
+    let body = {
+      'orderId':'',
+      'orderName': this.formCreateOrder.value.name,
+      'orderDesc': this.formCreateOrder.value.desc,
+      'orderNote': this.formCreateOrder.value.note,
+      'orderQuantity': +this.formCreateOrder.value.quantity,
+      'orderPrice': +this.formCreateOrder.value.price,
+      'customerId': this.customerId
+    }
+    this.xhr.post(url,body).subscribe((res: any) => {
+      if (res) {
+        this.lstOrder = res.result;
+      }
+    }, (err) => {
+
+    });
   }
 
 }
--- src/app/service/services/consume-apis.ts
+++ src/app/service/services/consume-apis.ts
@@ -1,11 +1,16 @@
 export class CONSUME_API {
 
-    static apiUrl: string = window['apiUrl'] || (location.protocol + '//' + 'localhost:8080/api');  
+    static apiUrl: string = window['apiUrl'] || (location.protocol + '//' + 'ec2-3-23-128-228.us-east-2.compute.amazonaws.com:8080/api');  
     static USERS: any = {
         LOGIN: '/v1/userCtrl/login',
     }
 
     static CUSTOMER: any = {
         CUSTOMERS: '/v1/customers',
+    }
+
+    static ORDER: any = {
+        CREATE_ORDER: '/v1/orderCtrl/createOrUpdateOrder',
+        GET_ORDER_BY_CUSTOMER_ID: '/v1/orderCtrl/getOrderByCutomerId'
     }
 }
